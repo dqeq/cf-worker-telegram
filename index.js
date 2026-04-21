344a427d60450ed92b7cf0580deb9e2af33cb3ca
Alexandr Vorobev <vorgovsky@gmail.com>
2026-04-21 21:10:58 +0300

    Update bot server URL for webhook updates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // CONFIG: Your bot server URL to receive webhook updates
-const BOT_UPDATE_FORWARD_URL = 'https://yourdomain.com/my-bot-handler'; // change this
+const BOT_UPDATE_FORWARD_URL = 'https://morning.vorgovsky.workers.dev/mbh';
 
 const TELEGRAM_API_BASE = 'https://api.telegram.org';
 
